5e9236b222bce135592791275fe03a5ab13ca4c9
Melissa Lee <melissalee@Melissas-MacBook-Pro-2.local>
2020-05-26 12:31:59 -0700

    deleting worksheets & tutorials

--- materials/tutorial_activity_02/setup.R
+++
@@ -1,7 +0,0 @@
-setup<-function() {connection <- dbConnect(RPostgres::Postgres(),
-                       dbname = "kickstarter",
-                       host = "r7k3-mds1.stat.ubc.ca",
-                       port = 5432,
-                       user = "dsci100",
-                       password = "dsci100")
-                    return (connection)}
